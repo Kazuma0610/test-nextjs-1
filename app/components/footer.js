6142dd48269c6dbcd2181274173c47148603598e
KazumaOchiai <kazuma06101209@gmail.com>
2025-04-29 22:10:36 +0900

    共通コードファイルの作成やCSS編集

--- app/components/footer.js
+++ app/components/footer.js
@@ -21,7 +21,7 @@ const Footer = () => {
                 <hr/>
                 <Link href="/blog">Blog</Link>
                 <Link href="/contact">Contact</Link>
-                <p>©{new Date().getFullYear()} Abe Hiroki</p>
+                <p>©{new Date().getFullYear()} Ochiai Kazuma</p>
             </div>
         </footer>
     )
