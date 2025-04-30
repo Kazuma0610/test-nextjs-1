a454d4b1ba68895e602200e5b227c900610a9392
KazumaOchiai <kazuma06101209@gmail.com>
2025-04-30 20:13:38 +0900

    ページネーション修正

--- app/utils/mdQueries.js
+++ app/utils/mdQueries.js
@@ -1,36 +1,34 @@
-// mdQueries.js
-
-import fs from "fs"
-import path from "path"
+import fs from "fs"       
+import path from "path" 
 import matter from "gray-matter"
 
-export const blogsPerpage = 5
+export const blogsPerPage = 5
 
-export async function getAllBlogs(){
-  const files = fs.readdirSync(path.join("data"))
-  const blogs = files.map((fileName) => {
-    const slug = fileName.replace(".md", "")
-    const fileData = fs.readFileSync(
-      path.join("data", fileName),
-      "utf-8"
-    )
-    const { data } = matter(fileData)
-    return {
-      frontmatter: data,
-      slug: slug,
+export async function getAllBlogs() {  
+    const files = fs.readdirSync(path.join("data"))
+    const blogs = files.map((fileName) => {
+        const slug = fileName.replace(".md", "")
+        const fileData = fs.readFileSync(
+            path.join("data", fileName),
+            "utf-8"
+        )
+        const { data } = matter(fileData) 
+        return {
+            frontmatter: data,
+            slug: slug, 
+        }
+    })
+
+    const orderedBlogs = blogs.sort((a, b) => {
+        return b.frontmatter.id - a.frontmatter.id
+    })
+
+    const numberPages = Math.ceil(orderedBlogs.length / blogsPerPage) 
+
+    return{
+        blogs: orderedBlogs,
+        numberPages: numberPages
     }
-  })
-
-  const orderedBlogs = blogs.sort((a, b) => {
-    return b.frontmatter.id - a.frontmatter.id
-  })
-
-  const numberPages = Math.ceil ( orderedBlogs.length / blogsPerpage)
-
-  return{
-    blogs: orderedBlogs,
-    numberPages: numberPages
-  }
 }
 
 export async function getSingleBlog(context) {
